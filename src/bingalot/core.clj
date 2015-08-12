689681435805a9dc19308863786187ed4369c4c6
Jessica Kerr <jessitron@outpace.com>
2015-08-12 01:17:12 +0000

    yay it fails to authorize
    but it compiles

--- src/bingalot/core.clj
+++ src/bingalot/core.clj
@@ -1,4 +1,17 @@
-(ns bingalot.core)
+(ns bingalot.core
+  (:require    [twitter.oauth :as oauth]
+               [twitter.callbacks]
+               [twitter.callbacks.handlers]
+               [twitter.api.restful :as rest]))
+
+(defn authenticate []  (oauth/make-oauth-creds
+                        (or (System/getenv "TWITTER_TOKEN") (throw (RuntimeException. "please define TWITTER_TOKEN")))
+                        (or (System/getenv "TWITTER_SECRET") "carrot") ))
+
+(defn -main []
+  (let [creds (authenticate)]
+       (rest/statuses-update :oauth-creds creds
+                             :params {:status "hello world"})))
 
 (defn foo
   "I don't do a whole lot."
